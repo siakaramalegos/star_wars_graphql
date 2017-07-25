9af2869614905932ffd9af51d0ca8354d6d4bcf1
Sia <siakaramalegos@gmail.com>
2017-07-24 19:00:38 -0500

    Adds starships to schema

--- schema.js
+++ schema.js
@@ -2,18 +2,35 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList
 } = require('graphql')
 const fetch = require('node-fetch')
 
 const BASE_URL = 'https://swapi.co/api'
 
+const StarshipType = new GraphQLObjectType({
+  name: 'Starship',
+  description: 'A Starship resource is a single transport craft that has hyperdrive capability.',
+  fields: () => ({
+    name: {type: GraphQLString},
+    model: {type: GraphQLString},
+    starship_class: {type: GraphQLString},
+  })
+})
+
 const PersonType = new GraphQLObjectType({
   name: 'Person',
-  description: 'character in the movies',
+  description: 'A People resource is an individual person or character within the Star Wars universe.',
   fields: () => ({
     name: {type: GraphQLString},
     birth_year: {type: GraphQLString},
     gender: {type: GraphQLString},
+    starships: {
+      type: new GraphQLList(StarshipType),
+      resolve: (person) => person.starships.map((ship) => (
+        fetch(ship).then(res => res.json())
+      ))
+    }
   })
 })
 
@@ -28,6 +45,16 @@ const QueryType = new GraphQLObjectType({
       },
       resolve: (root, args) => (
         fetch(`${BASE_URL}/people/${args.id}/`)
+          .then(res => res.json())
+      )
+    },
+    starship: {
+      type: StarshipType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve: (root, args) => (
+        fetch(`${BASE_URL}/starships/${args.id}/`)
           .then(res => res.json())
       )
     }
